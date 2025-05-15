daa2d1742ae92d5619e74e8b7d28ba68089d0ae7
JYOTISEEMA <139834125+JYOTISEEMA@users.noreply.github.com>
2025-05-15 05:49:00 +0000

    conversion of  datatype

---
+++ 01_basics/02_conversion_operation.js
@@ -0,0 +1,66 @@
+
+// let score=33;
+
+// console.log(score);
+// console.log(typeof score);
+// console.log(typeof  (score));
+
+// let score1= "33";
+// console.log(typeof score1);
+
+// // converting string to number
+// let newScore=Number (score1);
+// console.log(typeof newScore);
+
+
+// let score3="33abc";
+// let newScore1=Number(score3);
+// console.log(typeof newScore1);
+// console.log(newScore1);  //NaN 
+
+// let score4=null;
+// let newScore2=Number(score4);
+// console.log(typeof newScore2);
+// console.log(newScore2);  //0 
+
+// let score5=undefined;
+// let newScore3=Number(score5);
+// console.log(typeof newScore3);
+// console.log(newScore3);  //NaN
+
+//  i/p   o/p    type
+// "33" => 33    number
+//"33abc"=> NaN  number
+//true   => 1    number
+//false  => 0    number
+
+//*********************************************************************** */
+let isLoggedIn=1;
+let booleanisLoggedIn=Boolean (isLoggedIn);
+// console.log(typeof(booleanisLoggedIn));
+// console.log(booleanisLoggedIn);
+
+let isLoggedIn1="Isha";
+let booleanisLoggedIn1=Boolean (isLoggedIn1);
+console.log(typeof(booleanisLoggedIn1));
+console.log(booleanisLoggedIn1);
+
+//  i/p   o/p    type
+//  1     true   boolean
+//  0     false  boolean
+// ""     false  boolean
+//"Isha"  true   boolean
+
+let someNumber=33;
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
+//  i/p   o/p    type
+//  33    33     string
+
+
+
+
+
+
